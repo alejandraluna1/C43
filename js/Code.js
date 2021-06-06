2eeeea8460edbbed12270cf1fb6f357bd1cd9ef2
alejandraluna1 <81326858+alejandraluna1@users.noreply.github.com>
2021-06-06 11:38:47 -0500

    Update Code.js

--- js/Code.js
+++ js/Code.js
@@ -8,18 +8,17 @@ function clues() {
     textSize(15)
     text("R E V B A I L A", 100,50)
     fill("lightblue")
-    text("Hint: Always changing, not constant !!", 100,70)
+    text("Sugerencia: ¡Siempre cambia, nunca es constante!", 100,70)
 
     fill("white")
     textSize(15)
-    text("C U T N I F O N", 700,150)
+    text("C U N I F O N", 700,150)
     fill("lightblue")
-    text("Hint: Performs a particular task !!", 700,170)
+    text("Sugerencia: ¡Realiza tareas en particular!", 700,170)
 
     fill("white")
     textSize(15)
-    text("A T E D A S B A", 100,250)
+    text("E D A T E D O S B A S", 100,250)
     fill("lightblue")
-    text("Hint: Stores all information !!", 100,270)
-
+    text("Sugerencia: ¡Almacena toda la información!", 100,270)
 }
